afe14199136e8ec17a518d0aca95abf52ab72844
Kristian Bjørklund <kb@datalogik.dk>
2023-12-12 14:31:28 +0100

    Update nuget-packages

--- Lds.MongoRepositoryTests/DeleteProtectedTests.cs
+++ Lds.MongoRepositoryTests/DeleteProtectedTests.cs
@@ -5,40 +5,40 @@ using NUnit.Framework;
 
 namespace Lds.MongoRepositoryTests
 {
-  class DeleteProtectedTests
+  internal class DeleteProtectedTests
   {
-    private MongoRepository<Stubborn> repo;
+    private readonly MongoRepository<Stubborn> _repo;
 
     public DeleteProtectedTests()
     {
-      repo = Repository.For<Stubborn>();
+      _repo = Repository.For<Stubborn>();
     }
 
     [Test]
     public async Task Can_Delete_Item()
     {
       var entity = new Stubborn(null, "Will not die");
-      await repo.AddAsync(entity);
+      await _repo.AddAsync(entity);
 
-      Assert.IsNotNull(entity.Id);
-      Assert.IsNull(entity.Deleted);
+      Assert.That(entity.Id, Is.Not.Null);
+      Assert.That(entity.Deleted, Is.Null);
 
-      await repo.DeleteAsync(entity);
+      await _repo.DeleteAsync(entity);
     }
 
     [Test]
     public async Task Can_Not_Get_Deleted_Item()
     {
       var entity = new Stubborn(null, "Will not die");
-      await repo.AddAsync(entity);
+      await _repo.AddAsync(entity);
 
-      Assert.IsNotNull(entity.Id);
-      Assert.IsNull(entity.Deleted);
+      Assert.That(entity.Id, Is.Not.Null);
+      Assert.That(entity.Deleted, Is.Null);
 
-      await repo.DeleteAsync(entity);
+      await _repo.DeleteAsync(entity);
 
-      var tryToGet = await repo.GetByIdAsync(entity.Id);
-      Assert.IsNull(tryToGet);
+      var tryToGet = await _repo.GetByIdAsync(entity.Id);
+      Assert.That(tryToGet, Is.Null);
     }
 
 
@@ -46,37 +46,35 @@ namespace Lds.MongoRepositoryTests
     public async Task Can_Force_Get_Deleted_Item()
     {
       var entity = new Stubborn(null, "Will not die");
-      await repo.AddAsync(entity);
+      await _repo.AddAsync(entity);
 
-      Assert.IsNotNull(entity.Id);
-      Assert.IsNull(entity.Deleted);
+      Assert.That(entity.Id, Is.Not.Null);
+      Assert.That(entity.Deleted,Is.Null);
 
-      await repo.DeleteAsync(entity);
+      await _repo.DeleteAsync(entity);
 
-      var tryToGet = await repo.GetByIdAsync(entity.Id, true);
-      Assert.IsNotNull(tryToGet);
-      Assert.IsNotNull(tryToGet.Deleted);
+      var tryToGet = await _repo.GetByIdAsync(entity.Id, true);
+      Assert.That(tryToGet, Is.Not.Null);
+      Assert.That(tryToGet.Deleted, Is.Not.Null);
     }
 
     [Test]
     public async Task Can_Restore_Deleted_Item()
     {
       var entity = new Stubborn(null, "Will not die");
-      await repo.AddAsync(entity);
+      await _repo.AddAsync(entity);
 
-      Assert.IsNotNull(entity.Id);
-      Assert.IsNull(entity.Deleted);
+      Assert.That(entity.Id, Is.Not.Null);
+      Assert.That(entity.Deleted, Is.Null);
+      await _repo.DeleteAsync(entity);
 
-      await repo.DeleteAsync(entity);
+      var tryToGet = await _repo.GetByIdAsync(entity.Id);
+      Assert.That(tryToGet, Is.Null);
+      await _repo.RestoreAsync(entity.Id);
 
-      var tryToGet = await repo.GetByIdAsync(entity.Id);
-      Assert.IsNull(tryToGet);
-
-      await repo.RestoreAsync(entity.Id);
-
-      var tryToGetAgain = await repo.GetByIdAsync(entity.Id);
-      Assert.IsNotNull(tryToGetAgain);
-      Assert.IsNull(tryToGetAgain.Deleted);
+      var tryToGetAgain = await _repo.GetByIdAsync(entity.Id);
+      Assert.That(tryToGetAgain, Is.Not.Null);
+      Assert.That(tryToGetAgain.Deleted, Is.Null);
 
     }
 
